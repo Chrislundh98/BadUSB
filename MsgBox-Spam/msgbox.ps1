de0dac8fc7749ef7b9e5898b641a825ad3b15187
Chrislundh98 <christoffer.lundh98@gmail.com>
2024-03-11 19:22:42 +0100

    Update msgbox.ps1

--- MsgBox-Spam/msgbox.ps1
+++ MsgBox-Spam/msgbox.ps1
@@ -35,22 +35,29 @@ $onKeyUp = [System.Windows.Forms.KeyEventHandler]{
     }
 }
 
+class MyMessageFilter implements [System.Windows.Forms.IMessageFilter] {
+    [bool] PreFilterMessage([ref] [System.Windows.Forms.Message] $m) {
+        $WM_KEYDOWN = 0x100
+        $WM_KEYUP = 0x101
+
+        if ($m.Msg -eq $WM_KEYDOWN) {
+            $onKeyDown.Invoke($null, [System.Windows.Forms.KeyEventArgs]::new($m.WParam))
+        }
+        if ($m.Msg -eq $WM_KEYUP) {
+            $onKeyUp.Invoke($null, [System.Windows.Forms.KeyEventArgs]::new($m.WParam))
+        }
+        return $false
+    }
+}
+
 $form = New-Object System.Windows.Forms.Form
 $form.WindowState = 'Minimized'
 $form.ShowInTaskbar = $false
 
 $hook = New-Object System.Windows.Forms.NativeWindow
 $hook.AssignHandle((Get-Process -Id $pid).MainWindowHandle)
+$filter = [MyMessageFilter]::new()
+[System.Windows.Forms.Application]::AddMessageFilter($filter)
 
-[System.Windows.Forms.Application]::AddMessageFilter({
-    Param([System.Windows.Forms.Message]$m)
-    $WM_KEYDOWN = 0x100
-    $WM_KEYUP = 0x101
-
-    if ($m.Msg -eq $WM_KEYDOWN) { $onKeyDown.Invoke($null, [System.Windows.Forms.KeyEventArgs]::new($m.WParam)) }
-    if ($m.Msg -eq $WM_KEYUP) { $onKeyUp.Invoke($null, [System.Windows.Forms.KeyEventArgs]::new($m.WParam)) }
-
-    $false
-})
-
+# Run the application message loop
 [System.Windows.Forms.Application]::Run($form)
