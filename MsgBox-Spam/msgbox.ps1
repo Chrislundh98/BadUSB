0e3192458e474bc745d5ab3b32df2c13df66d038
Chrislundh98 <christoffer.lundh98@gmail.com>
2024-03-11 19:00:23 +0100

    Add files via upload

---
+++ MsgBox-Spam/msgbox.ps1
@@ -0,0 +1,56 @@
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+$leftCtrlPressed = $false
+$rightCtrlPressed = $false
+
+$onKeyDown = [System.Windows.Forms.KeyEventHandler]{
+    Param($sender, $e)
+
+    if ($e.KeyCode -eq [System.Windows.Forms.Keys]::ControlKey) {
+        if ($e.KeyData -band [System.Windows.Forms.Keys]::LControlKey) {
+            $leftCtrlPressed = $true
+        }
+        elseif ($e.KeyData -band [System.Windows.Forms.Keys]::RControlKey) {
+            $rightCtrlPressed = $true
+        }
+    }
+
+    if ($leftCtrlPressed -and $rightCtrlPressed) {
+        Start-Sleep -Seconds 5  # Wait for 5 seconds to check if both keys are still pressed.
+        if ($leftCtrlPressed -and $rightCtrlPressed) {
+            Get-Process | Where-Object {$_.ProcessName -eq 'msgbox'} | Stop-Process
+            $leftCtrlPressed = $false
+            $rightCtrlPressed = $false
+        }
+    }
+}
+
+$onKeyUp = [System.Windows.Forms.KeyEventHandler]{
+    Param($sender, $e)
+
+    if ($e.KeyCode -eq [System.Windows.Forms.Keys]::ControlKey) {
+        $leftCtrlPressed = $false
+        $rightCtrlPressed = $false
+    }
+}
+
+$form = New-Object System.Windows.Forms.Form
+$form.WindowState = 'Minimized'
+$form.ShowInTaskbar = $false
+
+$hook = New-Object System.Windows.Forms.NativeWindow
+$hook.AssignHandle((Get-Process -Id $pid).MainWindowHandle)
+
+[System.Windows.Forms.Application]::AddMessageFilter({
+    Param([System.Windows.Forms.Message]$m)
+    $WM_KEYDOWN = 0x100
+    $WM_KEYUP = 0x101
+
+    if ($m.Msg -eq $WM_KEYDOWN) { $onKeyDown.Invoke($null, [System.Windows.Forms.KeyEventArgs]::new($m.WParam)) }
+    if ($m.Msg -eq $WM_KEYUP) { $onKeyUp.Invoke($null, [System.Windows.Forms.KeyEventArgs]::new($m.WParam)) }
+
+    $false
+})
+
+[System.Windows.Forms.Application]::Run($form)
